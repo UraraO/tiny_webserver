5dcc38cf0ad87700bc1ea3640a777c26501a84bf
UraraO <Haru_UraraO@outlook.com>
2023-03-05 18:11:43 +0800

    delete ignore files

--- bin/thread_pool/thread_sync/CMakeFiles/locker.dir/locker.cpp.o.d
+++
@@ -1,3 +0,0 @@
-/root/remote_repo/tiny_WebServer/bin/thread_pool/thread_sync/CMakeFiles/locker.dir/locker.cpp.o: \
- /root/remote_repo/tiny_WebServer/src/thread_pool/thread_sync/locker.cpp \
- /usr/include/stdc-predef.h
